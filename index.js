89c5ffe70a638b83261ea06628167cfe6c732410
Nikki <nikkiweat@gmail.com>
2019-10-15 18:32:56 +0800

    troubleshoot webhook

--- index.js
+++ index.js
@@ -16,4 +16,46 @@ app.get("/mybot", function(req, res) {
   res.send("Wrong token!")
 })
 
+app.post("/webhook/", function(req, res) {
+  var messaging_events = req.body.entry[0].messaging
+  for (var i = 0; i < messaging_events.length; i++) {
+    var event = req.body.entry[0].messaging[i]
+    var sender = event.sender.id
+    if (event.message && event.message.text) {
+      var text = event.message.text
+      sendTextMessage(sender, text + "!")
+    }
+  }
+  res.sendStatus(200)
+})
+
+function sendTextMessage(sender, text) {
+  var messageData = {
+    text: text
+  }
+  request(
+    {
+      url: "https://graph.facebook.com/v2.6/me/messages",
+      qs: {
+        access_token: PAGE_ACCESS_TOKEN
+      },
+      method: "POST",
+      json: {
+        recipient: {
+          id: sender
+        },
+        message: messageData
+      }
+    },
+    function(error, response, body) {
+      if (!error) {
+        console.log("message sent from fb messenger!")
+        console.log(body)
+      } else {
+        console.error("Unable to send message:" + error)
+      }
+    }
+  )
+}
+
 app.listen(process.env.PORT || 8000, () => console.log("webhook is listening"))
